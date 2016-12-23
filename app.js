184bcd2b9f3b022b7a40a56a731fd23bcd1bdf58
Daniil <godastan@gmail.com>
2016-12-23 23:32:11 +0300

    mongoose

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const app = express();
 
 
 app
-    // view engine setup
+// view engine setup
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'pug')
 
     .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
     .use(logger('dev'))
     .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.urlencoded({extended: false}))
     .use(require('stylus').middleware(path.join(__dirname, 'public')))
     .use(express.static(path.join(__dirname, 'public')))
 
@@ -26,21 +26,21 @@ app
     .use('/users', users)
 
     // catch 404 and forward to error handler
-    .use(function(req, res, next) {
-      let err = new Error('Not Found');
-      err.status = 404;
-      next(err);
+    .use(function (req, res, next) {
+        let err = new Error('Not Found');
+        err.status = 404;
+        next(err);
     })
 
     // error handler
-    .use(function(err, req, res, next) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
+    .use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-      // render the error page
-      res.status(err.status || 500);
-      res.render('error');
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
     });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/local');
+
+const Post = mongoose.model('Post', {
+    title: String,
+    views: Number
+});
+
+router.get('/', function (req, res, next) {
+
+    let myPost = new Post({
+        title: 'Post Title',
+        views: 1337
+    });
+    myPost.save(err => console.log(err ? err : 'post saved'));
+
+    Post.find({}, function(err, data){
+        console.log(err ? err : data)
+    });
+
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
